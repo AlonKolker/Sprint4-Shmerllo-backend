7fb41d44a3d986674e42fdcde3f8b3f586203571
unknown <crazyguy771@gmail.com>
2022-07-29 19:29:33 +0300

    gitignore updated

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 
 
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-// 'dbURL': `mongodb+srv://alonkolker:Camaytaa@cluster0.xpj6c1a.mongodb.net/?retryWrites=true&w=majority`
+  // 'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': `mongodb+srv://alonkolker:Camaytaa@cluster0.xpj6c1a.mongodb.net/?retryWrites=true&w=majority`
 }

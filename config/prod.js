43a8c4ab1c7dec4f072fc770c69890f21b724657
Tal Hammer Topaz <talhtopaz@gmail.com>
2022-08-01 11:59:11 +0300

    update

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  // 'dbURL': `mongodb+srv://alonkolker:Camaytaa@cluster0.xpj6c1a.mongodb.net/?retryWrites=true&w=majority`
+  // 'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': `mongodb+srv://alonkolker:Camaytaa@cluster0.xpj6c1a.mongodb.net/?retryWrites=true&w=majority`
 }
